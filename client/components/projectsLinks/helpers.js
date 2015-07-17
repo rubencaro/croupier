5e352e31e5f7c3f5ba95f24f3e6a4e899afd0feb
Rubén Caro <ruben@elpulgardelpanda.com>
2015-07-17 07:26:41 +0200

    Main navigation

--- client/components/projectsLinks/helpers.js
+++ client/components/projectsLinks/helpers.js
@@ -1,6 +1,7 @@
 Template.projectsLinks.helpers({
 
   projects: function(){
+    if(this.list) return this.list;
     return Projects.find({_id: {$in: this.target.projects}},
                         {sort: {name: 1}}).fetch();
   }
